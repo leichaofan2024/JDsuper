a0176bf742c938234710d6e523db563b513e8388
leichaofan <leichaofan2024@gmail.com>
2017-03-20 23:01:21 +0800

    haha

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-    @order.user =curent_user
+    @order.user =current_user
     @order.total = current_cart.total_price
 
     if @order.save
@@ -18,6 +18,6 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
-  end 
+  end
 
 end
