7b13d4bc5baee514a1e9fac09ea14919ad6cd789
leichaofan <leichaofan2024@gmail.com>
2017-03-20 11:22:54 +0800

    deal the show bug

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   version :thumb do
-    proces resize_to_fill: [200,200]
+    process resize_to_fill: [200,200]
   end
   version :medium do
     process resize_to_fill: [400,400]
