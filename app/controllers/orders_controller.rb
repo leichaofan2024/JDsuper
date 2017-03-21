87ea22cb6491b2ae89e86e12558c6202e782ebe4
leichaofan <leichaofan2024@gmail.com>
2017-03-21 12:00:18 +0800

    add OrderMailer function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,6 +18,8 @@ class OrdersController < ApplicationController
         product_list.quantity = cart_item.quantity
         product_list.save
       end
+      current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
       redirect_to order_path(@order.token)
     else
       render "carts/chekcout"
@@ -33,7 +35,7 @@ class OrdersController < ApplicationController
       @order.set_payment_with!("wechat")
       @order.pay!
       redirect_to order_path (@order.token),notice:"使用微信付款成功！"
-    end 
+    end
 
   end
 
